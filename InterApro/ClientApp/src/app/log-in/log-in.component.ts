d743c4b211e7d3d254369e6f6c77831d52816998
Steven S <freelancers2010@gmail.com>
2020-03-18 21:34:54 -0600

    README

--- InterApro/ClientApp/src/app/log-in/log-in.component.ts
+++ InterApro/ClientApp/src/app/log-in/log-in.component.ts
@@ -27,7 +27,7 @@ export class LogInComponent implements OnInit {
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
       username: ['', Validators.required],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      password: ['', [Validators.required, Validators.minLength(5)]]
     });
   }
 
